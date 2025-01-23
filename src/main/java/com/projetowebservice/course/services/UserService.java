03e23cd36326d5e21a9edbf981549ae246a0ed10
Christian Cordeiro <christiancordeiro96@gmail.com>
2025-01-23 18:00:11 -0300

    Exception handling - delete

--- src/main/java/com/projetowebservice/course/services/UserService.java
+++ src/main/java/com/projetowebservice/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetowebservice.course.entities.User;
 import com.projetowebservice.course.repositories.UserRepository;
+import com.projetowebservice.course.services.exceptions.DataBaseException;
 import com.projetowebservice.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+	    if (!repository.existsById(id)) {
+	        throw new ResourceNotFoundException(id);
+	    }
+		try {
+			repository.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {

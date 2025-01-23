125ca6cbc7b7be02c86cab585359e0d55cce092e
Christian Cordeiro <christiancordeiro96@gmail.com>
2025-01-23 18:11:08 -0300

    Exception handling - update

--- src/main/java/com/projetowebservice/course/services/UserService.java
+++ src/main/java/com/projetowebservice/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetowebservice.course.repositories.UserRepository;
 import com.projetowebservice.course.services.exceptions.DataBaseException;
 import com.projetowebservice.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateDate(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateDate(User entity, User obj) {
